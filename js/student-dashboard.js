843e4631be615562d88d014bc6680d0385596b79
A-Rod <anirudh.amiable@gmail.com>
2016-02-15 18:25:49 -0800

    Committing all code at once

---
+++ js/student-dashboard.js
@@ -0,0 +1,55 @@
+
+
+$(document).ready(function() {
+
+	$('.row').hide();
+
+	$("#button-instructor-toggle-1").click(function() {
+		$("#assignment-body-1").toggle("slow", function() {
+			if( $("#assignment-body-1").is( ":hidden" ) ){
+				$("#button-instructor-toggle-1").removeClass("glyphicon-minus");
+				$("#button-instructor-toggle-1").addClass("glyphicon-plus");
+			}else if( $("#assignment-body-1").is( ":visible" ) ){
+				$("#button-instructor-toggle-1").removeClass("glyphicon-plus");
+				$("#button-instructor-toggle-1").addClass("glyphicon-minus");				
+			}
+		});
+	});
+
+
+	$("#button-instructor-toggle-2").click(function() {
+		$("#assignment-body-2").toggle("slow", function() {
+			if( $("#assignment-body-2").is( ":hidden" ) ){
+				$("#button-instructor-toggle-2").removeClass("glyphicon-minus");
+				$("#button-instructor-toggle-2").addClass("glyphicon-plus");
+			}else if( $("#assignment-body-2").is( ":visible" ) ){
+				$("#button-instructor-toggle-2").removeClass("glyphicon-plus");
+				$("#button-instructor-toggle-2").addClass("glyphicon-minus");				
+			}
+		});
+	});
+
+
+	$("#button-instructor-toggle-3").click(function() {
+		$("#assignment-body-3").toggle("slow", function() {
+			if( $("#assignment-body-3").is( ":hidden" ) ){
+				$("#button-instructor-toggle-3").removeClass("glyphicon-minus");
+				$("#button-instructor-toggle-3").addClass("glyphicon-plus");
+			}else if( $("#assignment-body-3").is( ":visible" ) ){
+				$("#button-instructor-toggle-3").removeClass("glyphicon-plus");
+				$("#button-instructor-toggle-3").addClass("glyphicon-minus");				
+			}
+		});
+	});
+
+	document.getElementById( 'scrollinto' ).scrollIntoView();
+	$("#TimeLineButton").click(function(){
+     	$(".row").hide();
+     	$(".timeline").show();
+	});
+
+	$("#ListButton").click(function(){
+     	$(".row").show();
+     	$(".timeline").hide();
+	});
+});
